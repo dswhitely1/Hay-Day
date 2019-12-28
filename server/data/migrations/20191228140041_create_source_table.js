9af821fd40edb6fe367a04a8a44a97949297058d
Donald Whitely <dswhitely1@gmail.com>
2019-12-28 14:07:28 -0500

    Added Sources Table

---
+++ server/data/migrations/20191228140041_create_source_table.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('sources', tbl=> {
+      tbl.increments();
+      tbl.string('name', 128).notNullable().unique();
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('sources');
+};
---
+++ server/knexfile.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+}
